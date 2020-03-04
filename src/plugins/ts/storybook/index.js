b1dc7263651fd0ff7fe95d9aaf8e0ee975264245
Nic Reichert <nicolas.jreichert@gmail.com>
2020-03-04 11:32:53 +0100

    Fix deps

--- src/plugins/ts/storybook/index.js
+++ src/plugins/ts/storybook/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const Package = require("../package");
+const Package = require("../../package");
 
 module.exports = async targetDir =>
   fs.copy(path.join(__dirname, "template"), targetDir).then(() => {
